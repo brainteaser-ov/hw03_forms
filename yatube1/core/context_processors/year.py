9853759e9e721aed74ddbf56e47a06963e68dcd3
Oxana Goncharova <226@MacBook-Pro-Oksana.local>
2022-06-23 12:12:22 +0300

    comment1

---
+++ yatube1/core/context_processors/year.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def year(request):
+    """Добавляет переменную с текущим годом."""
+    annum = datetime.now().year
+    return  {
+       'year': annum
+       }
---
+++ yatube1/posts/admin.py
@@ -0,0 +1,27 @@
+"""Models to manage the admin's account."""
+from django.contrib import admin
+from .models import Group, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    """Class used to manage posts in the admin's account."""
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group'
+    )
+    list_editable = ('group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    """Class used to manage groups in the admin's account."""
+    list_display = ('pk', 'title', 'slug', 'description')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
