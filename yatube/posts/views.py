ec0cd2108ca40e3af0309e20a16b106f4f2ce763
Oxana Goncharova <226@MacBook-Pro-Oksana.local>
2022-06-23 21:19:32 +0300

    comment2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -98,7 +98,7 @@ def post_edit(request, post_id):
                 return render(
                     request,
                     'posts/create_post.html',
-                    {'form': form, 'is_edit': True,'post': post}
+                    {'form': form, 'is_edit': True, 'post': post}
                 )
         else:
             form = PostForm(instance=post)
